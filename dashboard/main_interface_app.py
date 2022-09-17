305056095df4ddecd38f3bb47da3a556dbfc60f4
yonamg <softeng2006@gmail.com>
2022-09-17 17:41:52 +0300

    fix the error

--- dashboard/main_interface_app.py
+++ dashboard/main_interface_app.py
@@ -3,12 +3,13 @@ import logging
 import os,sys,io
 import pandas as pd
 
-def streamlit_app(app,data_pre,entity, news_scor):
+
+def streamlit_app(app,entity,news_score,data_prep):
     # Add all your application her
 
-    app.add_app("Prompt Design",data_pre.data_preparation,pd.DataFrame)
+    app.add_app("Prompt Design",data_prep.data_preparing,pd.DataFrame)
     app.add_app("Entity Extraction", entity.entity_extraction,pd.DataFrame)
-    app.add_app('News Artifact Scoring', news_scor.news_scoring, pd.DataFrame)
+    app.add_app('News Artifact Scoring', news_score.news_scoring, pd.DataFrame)
 
     # The main app
     app.run()
