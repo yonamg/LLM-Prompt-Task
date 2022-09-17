8df1616826ca42694f5147b5c22df9c9089de5ad
yonamg <softeng2006@gmail.com>
2022-09-17 21:45:27 +0300

    adding api_connection

--- flask/api.py
+++ flask/api.py
@@ -1,40 +1,85 @@
-import os
-import re
+import json
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
+import pandas as pd
+import sys
+import re, os
+sys.path.append('../scripts')
+#sys.path.append(os.path.abspath(os.path.join('..')))
 
-from flask import Flask, request, jsonify, render_template
+from preprocess import Processor
+from prompts import Predict
+from logger import Logger
+app_logger = Logger("api.log").get_app_logger()
+processor=Processor()
+predictor = Predict()
 
-# create an instance of the Flask class
-app = Flask(__name__)
+app = FastAPI()
 
-# list if items
-items = [{"item": "item1"}, {"item": "item2"}]
+@app.get("/check")
+def check():
+  return "Your API is up!"
 
-@app.route('/')
-def index():
-    """Render the index page."""
-    # Main page
-    return jsonify({
-                "status": "success",
-                "message": "Hello, world!"
-             })
+class Item(BaseModel):
+    name: str
+    description: str
 
-@app.route('/items', methods=['GET', 'POST'])
-def items_route():
-    """Items route."""
-    if request.method == 'POST':
-        # push the item to the list
-        items.append(request.get_json())
-        # return the created item
-        return jsonify({
-            "status": "success",
-            "item": request.get_json()
-        })
-        # return jsonify({"status": "success", "message": "Post item!"})
-    elif request.method == 'GET':
-        # return the list of items
-        return jsonify({"status": "success", "items": items})
-        # return jsonify({"status": "sucess", "message": "Get Route for items!"})
+@app.post("/news")
+def predict_news(item:Item):
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 22091))
-    app.run(host='129.0.0.1', debug=True, port=port)
+    return item
+
+
+@app.post("/jdentities")
+def create_upload_file(file: UploadFile = File()):
+    predictions=[]
+    try:
+        df=pd.read_csv(file.file)
+        # 
+        # prediction pipeline goes heere
+        #
+        
+        for ind in df.index:
+            prediction=""
+            prediction=predictor.predict_job(df)
+            res={}
+            res={
+                "Post":df['Description'],
+                "Entities":prediction
+            }
+            predictions.append(res)
+
+
+        
+    except Exception as e:
+        pass
+    return json.dumps(predictions)
+
+    
+@app.post("/bnewscore")
+def create_upload_file(file: UploadFile = File()):
+    predictions=[]
+    try:
+        df=pd.read_csv(file.file,encoding="utf8")
+        # 
+        # prediction pipeline goes heere
+        #
+        for ind in df.index:
+            try:
+                prediction=""
+                # predictions.append(pd.DataFrame(df.iloc[ind,:]).T.to_json())
+                prediction=predictor.predict_news(pd.DataFrame(df.iloc[ind,:]).T,'67034bd3-023e-4c81-bb67-ba75aa2612e1-ft')
+                res={}
+                res={
+                    "News":(df.loc[ind,'Description']),
+                    "Predicted_Score":re.findall("\d+\.*\d*", prediction)[0] if len(re.findall("\d+\.*\d*", prediction)) >0 else ""
+                }
+                predictions.append(res)
+            except Exception as e:
+                app_logger.error(f"cannot convert to json {e}")
+
+    except Exception as e:
+        app_logger.erro(f"api: {e}")
+        pass
+    return {"predictions":predictions}
--- scripts/prompts.py
+++ scripts/prompts.py
@@ -1,19 +1,176 @@
-class cohereExtractor():
-    def __init__(self, examples,co):
-        self.examples = examples
-        self.co = co
+import warnings
+from logger import Logger
+import cohere
+import pandas as pd
+from IPython.display import display
 
-    def make_prompt(self, example):
-        prompt = self.examples + [example]
+import sys
+warnings.simplefilter('ignore')
 
-        return ("".join([str(exam) for exam in prompt]))
+sys.path.append('../scripts')
+sys.path.append('../notebook')
+from preprocess import Processor
+import config
+api_key = config.cohere_api['api_key']
+processor=Processor()
 
-    def extract(self, example):
-        extraction = self.co.generate(
-            model='large',
-            prompt=self.make_prompt(example),
-            max_tokens=100,
-            temperature=0.5,
-            stop_sequences=["----"])
+class Predict:
+    def __init__(self):
+        """Initilize class."""
+        try:
+            self.logger = Logger("prompt_model.log").get_app_logger()
+            self.api_key=api_key
+            self.logger.info('Successfully initialized predictor Object')
+        except Exception:
+            self.logger.exception('Failed to processor util Object')
+            sys.exit(1)
+        
+    def predict(self,train_df:pd.DataFrame,test:pd.DataFrame, model="xlarge"):
 
-        return(extraction.generations[0].text[:-1])
+        test_df=test.drop(test.columns[len(test.columns)-1],axis=1)
+
+        prompt=""
+        
+        for ind in train_df.index:
+            prompt += "Task: Generate Analyst Average Score\n\n"
+            for col in train_df.columns:
+                prompt += f"{col}: {train_df.loc[ind,col]}\n\n"
+            prompt += "-- --\n\n"
+        
+        prompt += "Task: Generate Analyst Average Score\n\n"
+        for col in test_df.columns:
+            ind=test_df.index[0]
+            prompt += f"{col}: {test_df.loc[ind,col]}\n\n"
+
+        prompt += f"{test.columns[len(test.columns)-1]}: "
+        # prompt = "Task: Score News relevance\n\n"+prompt
+        
+        co = cohere.Client(f'{self.api_key}')
+        response = co.generate(
+        model=model,
+        prompt=prompt.strip(),
+        max_tokens=4,
+        temperature=0.9,
+        k=0,
+        p=0.75,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop_sequences=["-- --"],
+        return_likelihoods='NONE')
+        print('Prediction: {}'.format(response.generations[0].text))
+
+        return prompt
+
+    def extract_entities(self,train_df:pd.DataFrame,test:pd.DataFrame, model="xlarge"):
+
+        test_df=test.drop(test.columns[len(test.columns)-1],axis=1)
+
+        prompt=""
+
+        for ind in train_df.index:
+            prompt += "Task: Extract job description entites from this text\n\n"
+            
+
+            prompt += f"Description: {train_df.loc[ind,'document']}\n\n"
+            tokens = "\n\n".join(train_df.loc[ind,'tokens'].split(';'))
+            prompt += f"{tokens}\n\n"
+            prompt += "-- --\n\n"
+        
+        for ind in test_df.index:
+            prompt += "Task: Extract job description entites from this text\n\n"
+            
+
+            prompt += f"Description: {test_df.loc[ind,'document']}\n\n"
+            
+        co = cohere.Client(f'{self.api_key}')
+        response = co.generate(
+        model=model,
+        prompt=prompt.strip(),
+        max_tokens=50,
+        temperature=0.9,
+        k=0,
+        p=0.75,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop_sequences=["-- --"],
+        return_likelihoods='NONE')
+        print('Prediction: {}'.format(response.generations[0].text))
+        
+        return prompt
+
+    def predict_news(self,test:pd.DataFrame, model="xlarge"):
+
+        test_df=processor.prepare_text(test)
+        if "Analyst_Average_Score" in test_df.columns:
+            test_df.drop(['Analyst_Average_Score'],axis=1,inplace=True)
+        train_df=pd.read_csv('../data/processed_news.csv').sample()
+
+        prompt=""
+        
+        prompt += "Task: Generate Analyst Average Score\n\n"
+        for col in test_df.columns:
+            ind=test_df.index[0]
+            prompt += f"{col}: {test_df.loc[ind,col]}\n\n"
+
+        prompt += f"Analyst_Average_Score: "
+        # prompt = "Task: Score News relevance\n\n"+prompt
+        with open('../data/sent-prompt.txt', 'w', encoding="utf-8") as f:
+            f.write(prompt)
+        co = cohere.Client(f'{self.api_key}')
+        response = co.generate(
+        model=model,
+        prompt=prompt.strip(),
+        max_tokens=4,
+        temperature=0.9,
+        k=0,
+        p=0.75,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop_sequences=["-- --"],
+        return_likelihoods='NONE')
+    
+        prediction = (response.generations[0].text)
+        
+        return prediction
+
+
+    def predict_job(self,test:pd.DataFrame, model='xlarge'):
+        
+        train=pd.read_json('../data/job_description_train.json')
+        train_df=processor.prepare_job_description_text(train).sample()
+        test_df=processor.prepare_text(test)["document"]
+        # test_df.drop(test.columns[len(test.columns)-1],axis=1,inplace=True)
+
+        prompt=""
+
+        for ind in train_df.index:
+            prompt += "Task: Extract job description entites from this text\n\n"
+            
+
+            prompt += f"Description: {train_df.loc[ind,'document']}\n\n"
+            tokens = "\n\n".join(train_df.loc[ind,'tokens'].split(';'))
+            prompt += f"{tokens}\n\n"
+            prompt += "-- --\n\n"
+        
+        for ind in test_df.index:
+            prompt += "Task: Extract job description entites from this text\n\n"
+            
+
+            prompt += f"Description: {test_df.loc[ind,'document']}\n\n"
+            
+        co = cohere.Client(f'{self.api_key}')
+        response = co.generate(
+        model=model,
+        prompt=prompt.strip(),
+        max_tokens=50,
+        temperature=0.9,
+        k=0,
+        p=0.75,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop_sequences=["-- --"],
+        return_likelihoods='NONE')
+        
+        prediction = (response.generations[0].text)
+        
+        return prediction
